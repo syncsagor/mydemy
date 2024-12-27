7b333151a0bed68d0fe1527ba08994c8a4f63029
Sagor Ali <sagorbbr1@gmail.com>
2024-12-27 20:59:45 +0600

    add initial files of Mydemy website

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Nav from "./Components/Nav/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+    </div>
+  );
+}
+
+export default App;
